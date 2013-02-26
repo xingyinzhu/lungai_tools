137fb617e8211e00a520c3091174ff7d70f39b9f
xingyinzhu <xingyinzhu@gmail.com>
2013-02-26 16:16:21 +0800

    rename imagename
    Signed-off-by: xingyinzhu <xingyinzhu@gmail.com>

--- src/file.py
+++ src/file.py
@@ -15,6 +15,15 @@ def readWheelImageName(path):
         db.insertOneWheelIntoDB(conn, imagename)
     conn.commit()
 #-------------------------------------------------------------------------------
-
+def renameWheelImageName(path , postfix):
+    imagelist = os.listdir(path)
+    for image in imagelist:
+        imagename = image[0:image.find('.')] + postfix + image[image.find('.'):]
+        print image + " " + imagename
+        image = os.path.join(path,image)
+        imagename = os.path.join(path,imagename)
+        os.rename(image, imagename)
+#-------------------------------------------------------------------------------
 if __name__ == "__main__":
-    readWheelImageName(folder)
+    #readWheelImageName(folder)
+    renameWheelImageName("D:\\400\\", "_big")
